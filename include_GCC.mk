04306aa2546ac9e0b2d5af7a9078a53737a66133
Jan Eitzinger <unrz254@cshpc.rrze.uni-erlangen.de>
2019-03-13 14:46:35 +0100

    Fix typo in header

--- include_GCC.mk
+++ include_GCC.mk
@@ -1,9 +1,9 @@
 CC  = gcc
 LINKER = $(CC)
 
-OPENMP   = # -fopenmp
+OPENMP   =  -fopenmp
 CFLAGS   = -Ofast -std=c11 $(OPENMP)
 LFLAGS   = $(OPENMP)
 DEFINES  = -D_GNU_SOURCE
 INCLUDES =
-LIBS     =
+LIBS     = -lpthread
