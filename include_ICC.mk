bc717360ecb6e1a15bdff31b616109fc4ea358cc
Georg Hager <georg.hager@rrze.uni-erlangen.de>
2019-03-14 13:14:42 +0100

    removed -qopt-report from Intel flags

--- include_ICC.mk
+++ include_ICC.mk
@@ -5,7 +5,7 @@ ifeq ($(ENABLE_OPENMP),true)
 OPENMP   = -qopenmp
 endif
 
-CFLAGS   = -qopt-report -Ofast -xHost -std=c99 -ffreestanding $(OPENMP)
+CFLAGS   = -Ofast -xHost -std=c99 -ffreestanding $(OPENMP)
 LFLAGS   = $(OPENMP)
 DEFINES  = -D_GNU_SOURCE
 INCLUDES =
