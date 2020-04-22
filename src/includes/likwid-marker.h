3cb73d5ba01a78dbe5d0ee66e68fbce796f3470c
Jan Eitzinger <jan.eitzinger@fau.de>
2020-04-22 10:09:59 +0200

    Update marker header

--- src/includes/likwid-marker.h
+++ src/includes/likwid-marker.h
@@ -30,7 +30,16 @@
 
 #ifdef LIKWID_PERFMON
 #include <likwid.h>
-#else
+#define LIKWID_MARKER_INIT likwid_markerInit()
+#define LIKWID_MARKER_THREADINIT likwid_markerThreadInit()
+#define LIKWID_MARKER_SWITCH likwid_markerNextGroup()
+#define LIKWID_MARKER_REGISTER(regionTag) likwid_markerRegisterRegion(regionTag)
+#define LIKWID_MARKER_START(regionTag) likwid_markerStartRegion(regionTag)
+#define LIKWID_MARKER_STOP(regionTag) likwid_markerStopRegion(regionTag)
+#define LIKWID_MARKER_CLOSE likwid_markerClose()
+#define LIKWID_MARKER_RESET(regionTag) likwid_markerResetRegion(regionTag)
+#define LIKWID_MARKER_GET(regionTag, nevents, events, time, count) likwid_markerGetRegion(regionTag, nevents, events, time, count)
+#else  /* LIKWID_PERFMON */
 #define LIKWID_MARKER_INIT
 #define LIKWID_MARKER_THREADINIT
 #define LIKWID_MARKER_SWITCH
@@ -39,6 +48,7 @@
 #define LIKWID_MARKER_STOP(regionTag)
 #define LIKWID_MARKER_CLOSE
 #define LIKWID_MARKER_GET(regionTag, nevents, events, time, count)
-#endif
+#define LIKWID_MARKER_RESET(regionTag)
+#endif /* LIKWID_PERFMON */
 
 #endif /*LIKWID_MARKERS_H*/
