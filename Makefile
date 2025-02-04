6f958e35c62d59a1e4b9f702cea6ab4cea0901d4
Jan Eitzinger <jan@moebiusband.org>
2025-02-04 06:29:42 +0100

    Put correct project notice in copyright header

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 # Copyright (C) NHR@FAU, University Erlangen-Nuremberg.
-# All rights reserved.
+# All rights reserved. This file is part of TheBandwidthBenchmark.
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 
--- src/kernels.h
+++ src/kernels.h
@@ -1,5 +1,5 @@
 /* Copyright (C) NHR@FAU, University Erlangen-Nuremberg.
- * All rights reserved. This file is part of CG-Bench.
+ * All rights reserved. This file is part of TheBandwidthBenchmark.
  * Use of this source code is governed by a MIT style
  * license that can be found in the LICENSE file. */
 #ifndef __KERNELS_H_
