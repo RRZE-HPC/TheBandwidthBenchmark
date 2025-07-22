b7522c3f141f6445c1e6ab0962c0169cee7ed82d
Jan Eitzinger <jan@moebiusband.org>
2025-07-22 08:51:34 +0200

    Update deprecated CLANG flags

--- mk/include_CLANG.mk
+++ mk/include_CLANG.mk
@@ -9,7 +9,7 @@ OPENMP   = -fopenmp
 endif
 
 VERSION  = --version
-CFLAGS   = -Ofast -std=c99 $(OPENMP)
+CFLAGS   = -O3 -ffast-math -std=c99 $(OPENMP)
 #CFLAGS   = -Ofast -fnt-store=aggressive  -std=c99 $(OPENMP) #AMD CLANG
 LFLAGS   = $(OPENMP)
 DEFINES  = -D_GNU_SOURCE
